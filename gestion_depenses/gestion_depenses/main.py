9618a26eb4663b1299653aac1b28d02f8f56968c
Hermann002 <hermannnzeudeu@gmail.com>
2025-03-07 01:05:16 +0100

    correction

--- gestion_depenses/gestion_depenses/main.py
+++ gestion_depenses/gestion_depenses/main.py
@@ -11,23 +11,23 @@ def main():
     parser.add_argument('--amount', type=float, help="Expense amount")
     parser.add_argument('--cat', help="Expense category")
     parser.add_argument('--desc', help="Expense description")
-    parser.add_argument('--mounth', help="Months to filter expenses")
+    parser.add_argument('--month', help="Months to filter expenses")
 
     args = parser.parse_args()
 
     gestionnaire = GestionnaireDepenses(f"{DATA_DIR}/depenses.csv")
 
-    if args.action == 'add':
-        if not args.montant or not args.categorie:
+    if args.actions == 'add':
+        if not args.amount or not args.cat:
             parser.error("L'argument --montant et --categorie sont requis pour ajouter une depense")
-        gestionnaire.ajouter_depense(args.montant, args.categorie, args.description)
-    elif args.action == 'print':
+        gestionnaire.ajouter_depense(args.amount, args.cat, args.desc)
+    elif args.actions == 'print':
         gestionnaire.affiche_depenses()
-    elif args.action == 'filter':
-        if not args.categorie and not args.mois:
+    elif args.actions == 'filter':
+        if not args.cat and not args.month:
             parser.error("The --cat or --month argument is required to filter expenses.")
-        gestionnaire.filtrer_depenses(args.categorie, args.mois)
-    elif args.action == 'all':
+        gestionnaire.filtrer_depenses(args.cat, args.month)
+    elif args.actions == 'all':
         gestionnaire.tous_afficher()
     else:
         parser.print_help()
